71e7a1c4decc12f564d85070ed3c159dfc3615b9
Hendrik Mann <124534996+hendr1km@users.noreply.github.com>
2024-12-05 22:40:46 +0100

    sec 2

--- day1.go
+++ day1.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	file, _ := os.Open("data.txt")
+	file, _ := os.Open("data1.txt")
 	defer file.Close()
 
 	var column1 []int
@@ -30,6 +30,11 @@ func main() {
 	sort.Ints(column1)
 	sort.Ints(column2)
 
+	part1(column1, column2)
+	part2(column1, column2)
+}
+
+func part1(column1, column2 []int) {
 	sum := 0
 	for i := 0; i < len(column1); i++ {
 		diff := column1[i] - column2[i]
@@ -39,4 +44,21 @@ func main() {
 		sum += diff
 	}
 	fmt.Println(sum)
+}
+
+func part2(column1, column2 []int) {
+
+	m2 := make(map[int]int)
+
+	for _, num := range column2 {
+		m2[num]++
+	}
+
+	sim := 0
+	for _, num := range column1 {
+		sim += num * m2[num]
+	}
+
+	fmt.Println(sim)
+
 }
